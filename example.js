5e1d2a6407dff62781d1333ab5f1e4eba6ece644
shumaila-Rehman <160052356+shumaila-Rehman@users.noreply.github.com>
2024-03-08 19:54:23 +0500

    Add files

---
+++ example.js
@@ -0,0 +1,16 @@
+//                         =======> Alien Colors # 2 <========
+//   one version runs with if block
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("The player just earned 5 points");
+}
+else {
+    console.log("The player just earned 10 points");
+}
+// one version runs with else block
+if (alien_color === "yellow") {
+    console.log("The player just earned 5 points");
+}
+else {
+    console.log("The player just earned 10 points");
+}
---
+++ test.js
@@ -0,0 +1,32 @@
+//                    =======> Alien Colors # 3 <========
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("The player just earned 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("The player just earned 10 points");
+}
+else if (alien_color === "red") {
+    console.log("The player just earned 15 points");
+}
+var ball_color = "red";
+if (ball_color === "red") {
+    console.log("the player just earned 10 points");
+}
+else if (ball_color === "green") {
+    console.log(" The player just earned 5 points");
+}
+else if (ball_color === "yellow") {
+    console.log("The player just earned 15 points");
+}
+// 
+var basket_color = "yellow";
+if (basket_color === "yellow") {
+    console.log("The player just earned 15 points");
+}
+else if (basket_color === "red") {
+    console.log("The player just earned 10 points");
+}
+else if (basket_color === "green") {
+    console.log("The player just earned 5 points");
+}
